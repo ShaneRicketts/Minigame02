d13d5ac373ebd85ca59d5ab75481fe52251c76a6
LeveledRogue <shanerickettswork@gmail.com>
2021-12-01 16:22:15 -0500

    added a spawn manager script controlled but button press that spawns a random animal chosen from an index in a random position within set boundaries

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //random generate spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+            //randomly generate animal index
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
