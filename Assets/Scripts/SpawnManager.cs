117191a454137c4ddd181821ee6114cc905b4b1e
LeveledRogue <shanerickettswork@gmail.com>
2021-12-01 16:28:22 -0500

    changed spawn from button press to a timer using Invoke Repetaing

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,17 +7,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //random generate spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        //random generate spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
 
-            //randomly generate animal index
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+        //randomly generate animal index
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
     }
 }
