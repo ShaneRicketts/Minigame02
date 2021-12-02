057bee90b86317847d25e27dcc9b26a6a0817727
LeveledRogue <shanerickettswork@gmail.com>
2021-12-02 15:22:54 -0500

    fixed bugs supplied by unity course

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool canFetch;
+    private float initiateFetch = 0.1f;
+    public float fetchResetTime = 2;
 
-    // Update is called once per frame
+    void Start()
+    {
+        InvokeRepeating("AllowFetch", initiateFetch, fetchResetTime);
+    }
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send the dog to fetch
+        if (Input.GetKeyDown(KeyCode.Space) && canFetch)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canFetch = false;
         }
+    }
+
+    void AllowFetch()
+    {
+        canFetch = true;
     }
 }
